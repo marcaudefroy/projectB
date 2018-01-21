50b059f0171005bb6c418fa7d19fe0b6ae6913ad
Marc Audefroy <marc.audefroy@corp.ovh.com>
2018-01-21 21:32:43 +0100

    init C

--- models/bar.go
+++ models/bar.go
@@ -1,5 +1,15 @@
 package models
 
+import beez "github.com/marcaudefroy/projectC/models"
+
 //Bar bar
 type Bar struct {
+	B beez.Beez
+}
+
+//C c
+var C = Bar{
+	B: beez.Beez{
+		Name: "kk",
+	},
 }
